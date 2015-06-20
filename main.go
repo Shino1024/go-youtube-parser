19162389068041069c89f9a99d197164be407400
Shino1024 <justincompspec@gmail.com>
2015-06-20 22:42:22 +0200

    Began working on the program. The structures that will specify the HTML data which the program will care about are going to be definied.

---
+++ main.go
@@ -0,0 +1,34 @@
+//usr/bin/go build $0 $@; /usr/bin/go run $0 $@; exit;
+
+package main
+
+import (
+	"fmt"
+	"encoding/xml"
+	"net/http"
+)
+
+type Likes struct {
+}
+
+type Dislikes struct {
+}
+
+type Views struct {
+}
+
+type Description struct {
+}
+
+type Date struct {
+}
+
+type Category struct {
+}
+
+type Channel struct {
+}
+
+func main() {
+	
+}
